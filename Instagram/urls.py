47fd4e654758ed310e91766dd077316fc2dced0d
Ático <atico@202C11E29168968.local>
2017-11-04 12:54:18 -0500

    Tercer commit

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
 from. import views
 
 urlpatterns = [
     url (r'^$', views.index, name = 'index'),
-    url (r'^/login$', views.login, name = 'login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name= 'login.html', redirect_authenticated_user=True ), name ='login'),
+    url(r'^Logout/$', auth_views.LogoutView.as_view(), name = 'logout'),
+    url (r'^entro/$', views.entro, name = 'entro'),
+    url (r'^perfil/$', views.perfil, name = 'perfil'),
 ]
--- Instagram/views.py
+++ Instagram/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from Instagram.models import *
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index (request):
+    if request.user.is_authenticated:
+        return redirect ('entro')
     if request.method == 'GET':
         return render(request,'index.html')
     else:
@@ -10,16 +13,17 @@ def index (request):
         username = request.POST ['usuario']
         email = request.POST ['correo']
         password = request.POST ['password']
-        print (name)
-        print(username)
-        print (email)
-        print (password)
+
         usuarioDjango = User.objects.create_user(username= username, password = password, email =email , first_name = name )
         miUsuario = MiUsuario (usuario_django = usuarioDjango)
         usuarioDjango.save()
         miUsuario.save()
         return redirect ('login')
 
-def login (request):
-    print(MiUsuario.objects.count())
-    return render(request,'login.html')
+def entro (request):
+    return render(request,'entro.html')
+@login_required
+def perfil(request):
+    mi_usuario = MiUsuario.objects.get (pk= request.user.pk)
+    context = {'usuario_actual ': mi_usuario}
+    return render (request,'perfil.html',context)
